0b838df4edd0e8f0aad3dc85e34b9c4514a4f4db
marlonelima <marlonelimabr@gmail.com>
2021-07-12 11:45:34 -0300

    new backgrounds

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -9,10 +9,12 @@ interface IProps {
 }
 
 const backgrounds = {
-  morning: '/assets/conditions/morning.jpg',
-  sunny: '/assets/conditions/sunny.jpg',
-  afternoon: '/assets/conditions/afternoon.jpg',
-  night: '/assets/conditions/night.jpg'
+  morning: '/assets/conditions/morning.webp',
+  breakfast: '/assets/conditions/breakfast.webp',
+  day: '/assets/conditions/day.webp',
+  sunny: '/assets/conditions/sunny.webp',
+  afternoon: '/assets/conditions/afternoon.webp',
+  night: '/assets/conditions/night.webp'
 }
 
 export const BackgroundImage = ({ time }: IProps) => {
@@ -22,7 +24,9 @@ export const BackgroundImage = ({ time }: IProps) => {
     const hour = getHour(time)
 
     if (hour >= 0 && hour < 5) return setBackground(backgrounds.night)
-    if (hour >= 5 && hour < 12) return setBackground(backgrounds.morning)
+    if (hour >= 5 && hour <= 7) return setBackground(backgrounds.morning)
+    if (hour >= 8 && hour <= 9) return setBackground(backgrounds.breakfast)
+    if (hour >= 10 && hour <= 11) return setBackground(backgrounds.day)
     if (hour >= 12 && hour < 17) return setBackground(backgrounds.afternoon)
     if (hour >= 17 && hour < 19) return setBackground(backgrounds.sunny)
     if (hour >= 19) return setBackground(backgrounds.night)
